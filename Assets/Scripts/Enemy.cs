1f886dd1107a08d55b8d99bbbc15d37e531aa2ce
Damián Rojas <drojas@stilowg.local>
2021-11-02 11:43:00 -0300

    agregado de sonidos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Transform player;
-
     [SerializeField] int healt = 1, scorePoint = 100;
-
     [SerializeField] float speed = 1;
+    [SerializeField] AudioClip impactClip;
+
 
     private void Start()
     {
@@ -23,13 +23,14 @@ public class Enemy : MonoBehaviour
         EnemyIA();
         if (healt <=0) {
             GameManager.Instance.Score +=scorePoint;
-            Destroy(gameObject);
+            Destroy(gameObject, 0.1f);
         }
     }
 
     public void TakeDamage()
     {
         healt--;
+        AudioSource.PlayClipAtPoint(impactClip, transform.position);
     }
 
     void EnemyIA()
